136527c0af86a12a397cdd7c561c171f4515118f
AgrawalTrisha <agrawaltrisha@gmail.com>
2020-04-24 16:34:07 -0500

    Edited some nav bar colors Reorganized pages

--- lib/main.dart
+++ lib/main.dart
@@ -10,90 +10,13 @@
 // bar items. The first one is selected.](https://flutter.github.io/assets-for-api-docs/assets/material/bottom_navigation_bar.png)
 
 import 'package:flutter/material.dart';
+import 'pages/home.dart';
+import 'pages/welcome.dart';
 
-void main() => runApp(MyApp());
-
-/// This Widget is the main application widget.
-class MyApp extends StatelessWidget {
-  static const String _title = 'Flutter Code Sample';
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: _title,
-      home: MyStatefulWidget(),
-    );
+void main() => runApp(MaterialApp(
+  home: Home(),
+  routes: {
+    //'/': (context) => Home(),
+    '/welcome': (context) => Welcome(),
   }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  MyStatefulWidget({Key key}) : super(key: key);
-
-  @override
-  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'To be added: Fridge',
-      style: optionStyle,
-    ),
-    Text(
-      'To be added: Shopping List',
-      style: optionStyle,
-    ),
-    Text(
-      'To be added: Recipes',
-      style: optionStyle,
-    ),
-    Text(
-      'To be added: Barcode Scan',
-      style: optionStyle,
-    )
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('BottomNavigationBar Sample'),
-      ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.kitchen),
-            title: Text('Fridge'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_cart),
-            title: Text('Shopping List'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.local_dining),
-            title: Text('Recipes'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.camera_alt),
-            title: Text('Scan'),
-          )
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
-      ),
-    );
-  }
-}
+));
---
+++ lib/pages/home.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'To be added: Fridge',
+      style: optionStyle,
+    ),
+    Text(
+      'To be added: Shopping List',
+      style: optionStyle,
+    ),
+    Text(
+      'To be added: Recipes',
+      style: optionStyle,
+    ),
+    Text(
+      'To be added: Barcode Scan',
+      style: optionStyle,
+    )
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BottomNavigationBar Sample'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.kitchen),
+            title: Text('Fridge'),
+            backgroundColor: Colors.green[600],
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            title: Text('Shopping List'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.local_dining),
+            title: Text('Recipes'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.camera_alt),
+            title: Text('Scan'),
+          )
+        ],
+        currentIndex: _selectedIndex,
+        backgroundColor: Colors.green[600],
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.green[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
