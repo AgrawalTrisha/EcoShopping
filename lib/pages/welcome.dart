f3f2551bc87b46604212290b34c349c4bad82b16
AgrawalTrisha <agrawaltrisha@gmail.com>
2020-04-24 18:16:36 -0500

    Continued designing welcome page

--- lib/pages/welcome.dart
+++ lib/pages/welcome.dart
@@ -9,11 +9,34 @@ class _WelcomeState extends State<Welcome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: Center(
-          child: Image.asset('assets/ecoshopping-logo.png'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Center(
+              child: Text(
+                'Welcome to EcoShopping!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 40.0,
+                  letterSpacing: 2.0,
+                  color: Colors.white,
+                )
+              ),
+            ),
+            Image.asset('assets/ecoshopping-logo.png'),
+            Text(
+              "Our app aims to combat food waste by encouraging consumers to think before they buy. With features such as a smart shopping list, an inventory of what's already in your fridge, and recipe suggestions that don't require extra ingredients, EcoShopping asks consumers to consume only what they need and make the most out of what they have.",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 15.0,
+                letterSpacing: 0.5,
+                color: Colors.green[50],
+              )
+            )
+          ],
         ),
+
       ),
       backgroundColor: Colors.green[600],
     );
